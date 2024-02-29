cf8334b399cee4251f88818ccbfc92adf7d2a8b6
hymns12 <nickcloudsyinka1@gmail.com>
2024-02-29 11:47:12 -0500

    ipdata

--- contracts/RandomNumberG.sol
+++ contracts/RandomNumberG.sol
@@ -1,88 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./ERC20Token.sol";
-import "./IERC20Token.sol";
+// import "./ERC20Token.sol";
+// import "./IERC20Token.sol";
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
 
 contract RandomNumberG is VRFConsumerBase {
-    bytes32 internal keyHash;
-    uint256 internal fee;
-    uint256 public randomResult;
-    address public admin;
-    address public tokenAddress;
-    uint256 public totalParticipants;
-    uint256 public totalPrize;
-    mapping(uint256 => address) public participants;
-    mapping(address => uint256) public participantIndex;
-    bool public prizeDistributed;
+    event ChainFlip(uint256 requcistId);
+    event ChainFlipSnder(uint requcistId, bool diwin);
 
-    event PrizeDistributed(address winner, uint256 amount);
+    struct ChainFlipStatl {
+        uint256 fees;
+        uint256 RandomWorld;
+        address player;
+        bool didWin;
+        bool fulfull;
+        chainFlipsElltion picker;
+    }
+    
+    address constant linkAddress = 0xb0897686c545045aFc77CF20eC7A532E3120E0F1
+    address constant vrfWrapperAddress = 0xb0897686c545045aFc77CF20eC7A532E3120E0F1
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can call this function");
-        _;
+    struct gesAtt {
+        uint id;
+        string name;
+        bool false;
     }
 
-    constructor(
-        address _vrfCoordinator,
-        address _linkToken,
-        bytes32 _keyHash,
-        uint256 _fee,
-        address _tokenAddress
-    )
-        VRFConsumerBase(_vrfCoordinator, _linkToken)
-    {
-        keyHash = _keyHash;
-        fee = _fee;
-        admin = msg.sender;
-        tokenAddress = _tokenAddress;
+    gesAtt[] public ArrayGesAtt;
+
+    mapping (address => gasAtt) public newGesAtt;
+
+    enum chainFlipsElltion {
+      
+      upSide,
+      downSide
     }
 
-    function participate() external {
-        require(!prizeDistributed, "Prize already distributed");
-        require(participantIndex[msg.sender] == 0, "Already participated");
-        
-        totalParticipants++;
-        participants[totalParticipants] = msg.sender;
-        participantIndex[msg.sender] = totalParticipants;
-    }
+    mapping (uint256 => ChainFlipStatl) public statuess;
 
-    function distributePrize() external onlyAdmin {
-        require(totalParticipants > 0, "No participants");
-        require(!prizeDistributed, "Prize already distributed");
+    uint constant callBacklink = 1_000_000;
+    uint constant nmbers = 1;
+    uint constant requireNmuber = 3;
 
-        prizeDistributed = true;
-        bytes32 requestId = requestRandomness(keyHash, fee);
-        // You could store the requestId to verify the randomness later
-    }
+    constructor() VRFV2WrapperConsumerBase(linkAddress, vrfWrapperAddress) {}
 
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        randomResult = randomness;
-        uint256 winnerIndex = randomness % totalParticipants + 1;
-        address winner = participants[winnerIndex];
-        // Distribute prize to winner
-        distributeTokens(winner);
-        emit PrizeDistributed(winner, totalPrize);
-    }
+    function flipChain(chainFlipsElltion picker)
+    external payable returns (uint256) 
+    {}
 
-    function distributeTokens(address winner) internal {
-        IERC20 token = IERC20(tokenAddress);
-        uint256 balance = token.balanceOf(address(this));
-        require(balance >= totalPrize, "Not enough tokens to distribute");
+   function userIdente(uint _id, string _name) external returns (bool) {
+        gesAtt memory funGesAtt;
+        funGesAtt.id = _id;
+        funGesAtt.name = _name;
 
-        token.transfer(winner, totalPrize);
-    }
+        ArrayGesAtt.push(funGesAtt);
+        return true;
+   }
 
-    function setTotalPrize(uint256 _totalPrize) external onlyAdmin {
-        totalPrize = _totalPrize;
-    }
+   function usersrtRandom(uint _theRandom) external returns {
+    require(msg.sender != address(0), "Address Zero Not Valid");
 
-    function withdrawLink(address _to, uint256 _amount) external onlyAdmin {
-        IERC20 link = IERC20(address(chainlinkTokenAddress()));
-        require(link.transfer(_to, _amount), "Unable to transfer");
-    }
+        uint requireNmuber = requestRandomness();
+        if () {
+            
+        }
+   }
+
+
 }
 
